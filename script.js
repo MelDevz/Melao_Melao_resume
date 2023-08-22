afa6a3d0b633b6332dc545c032a78335f20aada1
Melao <121197442+MelDevz@users.noreply.github.com>
2023-08-22 12:00:51 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const navLinks = document.querySelectorAll('.nav_link');
-
-navLinks.forEach(navLink => {
-    navLinks.addEventListener('click', () => {
-        navLinks.classList.add('active');
-    });
-});
